dc087bec0878705f57dc40caecb70b73504ed3b4
Emrah Korkmaz <emrahkrkmz1@gmail.com>
2022-12-10 00:23:05 +0200

    add changes

---
+++ Sources/KiwanukaLogger/Helper/File.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Emrah Korkmaz on 10.12.2022.
+//
+
+import Foundation
+
+public extension KiwanukaLogger {
+    var description: String {
+        "Kiwanuka Logger"
+    }
+    
+    var version: String {
+        "v0.0.1"
+    }
+}
